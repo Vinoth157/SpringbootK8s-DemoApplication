3ac3dcf14b4f8e7a51561c27f495bf1422109334
Vinoth <iambalavinoth@gmail.com>
2022-01-16 20:48:56 +0530

    Initial Commit - Cloud Config

---
+++ CloudConfig/CloudConfig/src/test/java/com/vinoth/springbootk8s/studentapplication/CloudConfig/CloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinoth.springbootk8s.studentapplication.CloudConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
