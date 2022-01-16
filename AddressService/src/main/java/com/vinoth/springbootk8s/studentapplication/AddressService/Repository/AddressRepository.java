6b5ce8dc26bd5c9ce1fe5665d36add2ba3654dd7
Vinoth157 <39757168+Vinoth157@users.noreply.github.com>
2022-01-16 20:14:00 +0530

    Delete AddressService directory

--- AddressService/src/main/java/com/vinoth/springbootk8s/studentapplication/AddressService/Repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vinoth.springbootk8s.studentapplication.AddressService.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vinoth.springbootk8s.studentapplication.AddressService.Entity.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-
-}
