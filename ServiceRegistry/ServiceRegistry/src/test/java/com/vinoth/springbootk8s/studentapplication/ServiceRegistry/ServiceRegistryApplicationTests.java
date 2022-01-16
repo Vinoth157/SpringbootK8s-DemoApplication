8cb5ddbc870c1bbae3516d088f92210b15aad679
Vinoth <iambalavinoth@gmail.com>
2022-01-16 20:45:34 +0530

    Initial Commit - Service Registry

---
+++ ServiceRegistry/ServiceRegistry/src/test/java/com/vinoth/springbootk8s/studentapplication/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinoth.springbootk8s.studentapplication.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
